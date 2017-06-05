bd283bdfc9b43829ed1c7df8cade30cc184c9fbc
Andrew Bibby <andrew.bibby@yudu.com>
2017-06-05 14:07:43 +0100

    Initial commit - Added files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	var query = getUrlVars();
+
+	if(query['image']){
+		$('body').css('background-image', 'url(images/' + query['image'] + ')');
+	}
+
+	if(query['link']){
+		$('body').on('click', function(){
+			window.location = query['link'];
+		});
+	}
+
+	function getUrlVars(){
+	    var vars = [], hash;
+	    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	    for(var i = 0; i < hashes.length; i++){
+	        hash = hashes[i].split('=');
+	        vars.push(hash[0]);
+	        vars[hash[0]] = hash[1];
+	    }
+	    return vars;
+	}
+
+});
+
+
